48385bda2a3a8193dbb4f9ac3e23b00652628b5f
JACKn1993 <jack.disney1993@gmail.com>
2022-12-22 20:37:54 +0900

    ActiveHashのモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
+  belongs_to :condetion
+  belongs_to :shipping
+  belongs_to :prefecture
+  belongs_to :until_shipping
 end
