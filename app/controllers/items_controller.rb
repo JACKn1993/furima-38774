12e691a190206b9d4cc4a4d027c855d14180085f
JACKn1993 <jack.disney1993@gmail.com>
2022-12-26 21:15:22 +0900

    商品編集ページへ遷移できるユーザーを制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :set_item_params, only: [:show, :edit, :update]
+  before_action :anlyze_user, only: :edit
 
   def index
     @items = Item.includes( :user ).order("created_at DESC")
@@ -19,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -38,6 +37,16 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_id, :prefecture_id, :until_shipping_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item_params
+    @item = Item.find(params[:id])
+  end
+
+  def anlyze_user
+    unless @item.user == current_user
+      redirect_to root_path
+    end
   end
 end
 
