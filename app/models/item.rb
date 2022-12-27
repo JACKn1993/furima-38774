1904d518dc196076f1f89a7e501449ace55817f6
JACKn1993 <jack.disney1993@gmail.com>
2022-12-27 12:17:20 +0900

    itemモデルのバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,6 +17,6 @@ class Item < ApplicationRecord
   validates :shipping_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :until_shipping_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true,format: { with: /\A[0-9]+\z/ }, numericality: {only_integer: true, greater_than: 300,less_than: 9999999}
+  validates :price, presence: true,format: { with: /\A[0-9]+\z/ }, numericality: {only_integer: true, greater_than: 299,less_than: 9999999}
 
 end
