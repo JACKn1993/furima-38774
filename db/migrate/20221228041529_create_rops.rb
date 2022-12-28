ee8b775bbf21ff0a3ba68addfa3f1e35a6f4f53d
JACKn1993 <jack.disney1993@gmail.com>
2022-12-28 13:44:38 +0900

    マイグレーションの実行

--- db/migrate/20221228041529_create_rops.rb
+++ db/migrate/20221228041529_create_rops.rb
@@ -1,7 +1,8 @@
 class CreateRops < ActiveRecord::Migration[6.0]
   def change
     create_table :rops do |t|
-
+      t.integer :user_id, null: false
+      t.integer :item_id, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_22_104606) do
+ActiveRecord::Schema.define(version: 2022_12_28_041555) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,19 @@ ActiveRecord::Schema.define(version: 2022_12_22_104606) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "municipality", null: false
+    t.string "address", null: false
+    t.string "build_name"
+    t.string "tel", null: false
+    t.bigint "rop_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["rop_id"], name: "index_addresses_on_rop_id"
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
@@ -46,6 +59,13 @@ ActiveRecord::Schema.define(version: 2022_12_22_104606) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "rops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -67,5 +87,6 @@ ActiveRecord::Schema.define(version: 2022_12_22_104606) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "rops"
   add_foreign_key "items", "users"
 end
